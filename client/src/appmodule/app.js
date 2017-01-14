bfe4c809dc8a1cea46b0005024e163acbe7452ce
Seth Koch <sethkoch1@gmail.com>
2017-01-14 07:52:21 -0600

    added line to run in app.js that checks if user is not authenticated, then clear localStorage.  This might fix the token expired but localStorage not cleared bug

--- client/src/appmodule/app.js
+++ client/src/appmodule/app.js
@@ -1,7 +1,7 @@
 angular.module('easyismath', ['auth0.lock', 'angular-jwt', 'ui.router'])
 
 
-  .run(['$rootScope', 'authService', 'lock', 'authManager', function ($rootScope, authService, lock, authManager) {
+  .run(['$rootScope', 'authService', 'lock', 'authManager', '$window', function ($rootScope, authService, lock, authManager, $window) {
     // Put the authService on $rootScope so its methods
     // can be accessed from the nav bar
     $rootScope.authService = authService;
@@ -10,8 +10,10 @@ angular.module('easyismath', ['auth0.lock', 'angular-jwt', 'ui.router'])
     // set up in auth.service.js
     authService.registerAuthenticationListener();
 
-    //checks token validity after a refresh
+    //checks token validity after a refresh - lib/angular-jwt/src/services/authManager.js
     authManager.checkAuthOnRefresh();
+    //I added this so if the token is expired then local storage will be cleared
+    if($rootScope.isAuthenticated) $window.localStorage.clear();
 
     // Register the synchronous hash parser
     // when using UI Router
